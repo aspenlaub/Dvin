44674c01dc0a13ea2c288f544bdc8e2d7bbc1ec4
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-09-27 17:18:07 +0200

    Remove windows service functionality

--- src/Extensions/WebHostBuilderExtensions.cs
+++ src/Extensions/WebHostBuilderExtensions.cs
@@ -1,20 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using Aspenlaub.Net.GitHub.CSharp.Dvin.Repositories;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.WindowsServices;
 // ReSharper disable UnusedMember.Global
 
 namespace Aspenlaub.Net.GitHub.CSharp.Dvin.Extensions {
     public static class WebHostBuilderExtensions {
-        private static bool IsService(IEnumerable<string> mainProgramArgs) {
-            return !(Debugger.IsAttached || mainProgramArgs.Contains("--console"));
-        }
-
-        public static IWebHostBuilder UseDvin(this IWebHostBuilder builder, string dvinAppId, string[] mainProgramArgs) {
+        public static IWebHostBuilder UseDvin(this IWebHostBuilder builder, string dvinAppId, bool release, string[] mainProgramArgs) {
             var dvinRepository = new DvinRepository();
 
             var dvinApp = dvinRepository.LoadAsync(dvinAppId).Result;
@@ -22,26 +13,14 @@ namespace Aspenlaub.Net.GitHub.CSharp.Dvin.Extensions {
                 throw new Exception($"Dvin app {dvinAppId} not found");
             }
 
-            var port = IsService(mainProgramArgs) ? dvinApp.ReleasePort : dvinApp.DebugPort;
+            var port = release ? dvinApp.ReleasePort : dvinApp.DebugPort;
             builder.UseUrls($"http://localhost:{port}");
-            if (!IsService(mainProgramArgs)) {
-                return builder;
-            }
-
-            var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
-            var pathToContentRoot = Path.GetDirectoryName(pathToExe);
-            builder.UseContentRoot(pathToContentRoot);
             return builder;
         }
 
         public static void RunHost(this IWebHostBuilder builder, string[] mainProgramArgs) {
             var host = builder.Build();
-
-            if (IsService(mainProgramArgs)) {
-                host.RunAsService();
-            } else {
-                host.Run();
-            }
+            host.Run();
         }
     }
 }
