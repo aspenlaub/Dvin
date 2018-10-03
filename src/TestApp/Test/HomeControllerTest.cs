509f075d8fc3c76f0cc7c5d03a5d307e61aec6bc
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-10-03 08:23:24 +0200

    Add controller test

---
+++ src/TestApp/Test/HomeControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Aspenlaub.Net.GitHub.CSharp.Dvin.Repositories;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Dvin.TestApp.Test {
+    [TestClass]
+    public class TvinstControllerTest {
+        internal static HttpClient CreateHttpClient() {
+            var args = new string[] { };
+            var builder = Program.CreateWebHostBuilder(args);
+            var server = new TestServer(builder);
+            return server.CreateClient();
+        }
+
+        [TestMethod]
+        public async Task CanCreateTestClient() {
+            var repository = new DvinRepository();
+            var dvinApp = await repository.LoadAsync(Constants.DvinSampleAppId);
+            Assert.IsNotNull(dvinApp);
+            var url = $"http://localhost:{dvinApp.Port}/Home";
+
+            using (var client = CreateHttpClient()) {
+                Assert.IsNotNull(client);
+                var response = await client.GetAsync(url);
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+                var content = await response.Content.ReadAsStringAsync();
+                Assert.IsTrue(content.Contains("Hello World says your dvin app"));
+            }
+        }
+    }
+}
