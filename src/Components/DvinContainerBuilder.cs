a5b215d33686298a830483d79fd3bae068bf95f8
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-10-16 11:52:47 +0200

    Services configuration and web host builder extensions moved to DvinStandard and DvinCore

--- src/Components/DvinContainerBuilder.cs
+++ src/Components/DvinContainerBuilder.cs
@@ -3,7 +3,6 @@ using Aspenlaub.Net.GitHub.CSharp.Dvin.Repositories;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
 using Autofac;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Aspenlaub.Net.GitHub.CSharp.Dvin.Components {
     public static class DvinContainerBuilder {
@@ -11,13 +10,6 @@ namespace Aspenlaub.Net.GitHub.CSharp.Dvin.Components {
             builder.UsePegh(csArgumentPrompter);
             builder.RegisterType<DvinRepository>().As<IDvinRepository>();
             return builder;
-        }
-
-        // ReSharper disable once UnusedMember.Global
-        public static IServiceCollection UseDvinAndPegh(this IServiceCollection services, ICsArgumentPrompter csArgumentPrompter) {
-            services.UsePegh(csArgumentPrompter);
-            services.AddTransient<IDvinRepository, DvinRepository>();
-            return services;
         }
     }
 }
