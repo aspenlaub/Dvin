dc98ead33aff101460c4a7d0cf37b4d68136ebe5
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-10-16 10:21:32 +0200

    Changed test app ID

--- src/Test/Constants.cs
+++ src/Test/Constants.cs
@@ -1,5 +1,5 @@
 ﻿namespace Aspenlaub.Net.GitHub.CSharp.Dvin.Test {
     public class Constants {
-        public const string DvinSampleAppId = "Dvin.TestApp";
+        public const string DvinSampleAppId = "DvinTestApp";
     }
 }
