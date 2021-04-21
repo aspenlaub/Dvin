263c574692c0ae96b4f46a92ebcd8ae55bcf629f
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2021-04-21 16:40:32 +0200

    No need for separate DvinStandard/-Core any longer

--- src/Components/DvinContainerBuilder.cs
+++ src/Components/DvinContainerBuilder.cs
@@ -2,7 +2,9 @@
 using Aspenlaub.Net.GitHub.CSharp.Dvin.Repositories;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
+using Aspenlaub.Net.GitHub.CSharp.PeghCore;
 using Autofac;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Aspenlaub.Net.GitHub.CSharp.Dvin.Components {
     public static class DvinContainerBuilder {
@@ -10,6 +12,12 @@ namespace Aspenlaub.Net.GitHub.CSharp.Dvin.Components {
             builder.UsePegh(csArgumentPrompter);
             builder.RegisterType<DvinRepository>().As<IDvinRepository>();
             return builder;
+        }
+
+        public static IServiceCollection UseDvinAndPegh(this IServiceCollection services, ICsArgumentPrompter csArgumentPrompter) {
+            services.UsePegh(csArgumentPrompter);
+            services.AddTransient<IDvinRepository, DvinRepository>();
+            return services;
         }
     }
 }
---
+++ src/Extensions/WebHostBuilderExtensions.cs
@@ -0,0 +1,37 @@
+using System;
+using Aspenlaub.Net.GitHub.CSharp.Dvin.Components;
+using Aspenlaub.Net.GitHub.CSharp.Dvin.Interfaces;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Entities;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Extensions;
+using Autofac;
+using Microsoft.AspNetCore.Hosting;
+// ReSharper disable UnusedMember.Global
+
+namespace Aspenlaub.Net.GitHub.CSharp.Dvin.Extensions {
+    public static class WebHostBuilderExtensions {
+        public static IWebHostBuilder UseDvinAndPegh(this IWebHostBuilder builder, string dvinAppId, bool release, string[] mainProgramArgs) {
+            var containerBuilder = new ContainerBuilder().UseDvinAndPegh(new DummyCsArgumentPrompter());
+            var container = containerBuilder.Build();
+            var dvinRepository = container.Resolve<IDvinRepository>();
+
+            var errorsAndInfos = new ErrorsAndInfos();
+            var dvinApp = dvinRepository.LoadAsync(dvinAppId, errorsAndInfos).Result;
+            if (dvinApp == null) {
+                throw new Exception($"Dvin app {dvinAppId} not found");
+            }
+
+            if (errorsAndInfos.AnyErrors()) {
+                throw new Exception(errorsAndInfos.ErrorsToString());
+            }
+
+            builder.UseUrls($"http://localhost:{dvinApp.Port}");
+            return builder;
+        }
+
+        public static void RunHost(this IWebHostBuilder builder, string[] mainProgramArgs) {
+            var host = builder.Build();
+            host.Run();
+        }
+    }
+}
