bab282c59d7bd3df7fcfd1ddb46385138723f216
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2022-05-11 10:09:13 +0200

    Exclude folders that cannot be read

--- src/Components/FileSystemService.cs
+++ src/Components/FileSystemService.cs
@@ -9,7 +9,26 @@ using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
 namespace Aspenlaub.Net.GitHub.CSharp.Dvin.Components {
     public class FileSystemService : IFileSystemService {
         public IList<string> ListFilesInDirectory(IFolder folder, string pattern, SearchOption searchOption) {
-            return folder.Exists() ? Directory.GetFiles(folder.FullName, pattern, searchOption).ToList() : new List<string>();
+            return folder.Exists() ? ListFilesInDirectory(folder.FullName, pattern, searchOption).ToList() : new List<string>();
+        }
+
+        private IList<string> ListFilesInDirectory(string fullFolderName, string pattern, SearchOption searchOption) {
+            List<string> result;
+            try {
+                result = Directory.GetFiles(fullFolderName, pattern, SearchOption.TopDirectoryOnly).ToList();
+            } catch {
+                if (searchOption == SearchOption.TopDirectoryOnly) {
+                    throw;
+                }
+
+                return new List<string>();
+            }
+
+            foreach (var directory in Directory.GetDirectories(fullFolderName)) {
+                result.AddRange(ListFilesInDirectory(directory, pattern, SearchOption.AllDirectories));
+            }
+
+            return result;
         }
 
         public string ReadAllText(string fileName) {
---
+++ src/Test/Components/FileSystemServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Aspenlaub.Net.GitHub.CSharp.Dvin.Components;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Dvin.Test.Components {
+    [TestClass]
+    public class FileSystemServiceTest {
+        [TestMethod]
+        public void CanListFilesInDirectory() {
+            var location = GetType().Assembly.Location;
+            location = location.Substring(0, location.LastIndexOf('\\'));
+            var folder = new Folder(location);
+            var sut = new FileSystemService();
+            var files = sut.ListFilesInDirectory(folder, "*.*", SearchOption.AllDirectories);
+        }
+    }
+}
