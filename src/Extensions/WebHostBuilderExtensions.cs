68507974beea4e9b096053ba8108e38ea7aa121f
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-20 16:09:53 +0100

    Look up dvin app / port

--- src/Extensions/WebHostBuilderExtensions.cs
+++ src/Extensions/WebHostBuilderExtensions.cs
@@ -1,13 +1,7 @@
-using System;
 using System.Threading.Tasks;
-using Aspenlaub.Net.GitHub.CSharp.Dvin.Components;
-using Aspenlaub.Net.GitHub.CSharp.Dvin.Entities;
 using Aspenlaub.Net.GitHub.CSharp.Dvin.Interfaces;
-using Aspenlaub.Net.GitHub.CSharp.Skladasu.Entities;
-using Aspenlaub.Net.GitHub.CSharp.Skladasu.Extensions;
-using Autofac;
+using Aspenlaub.Net.GitHub.CSharp.Dvin.Singletons;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
 
 // ReSharper disable UnusedMember.Global
 
@@ -15,24 +9,8 @@ namespace Aspenlaub.Net.GitHub.CSharp.Dvin.Extensions;
 
 public static class WebHostBuilderExtensions {
     public static async Task<IWebHostBuilder> UseDvinAndPeghAsync(this IWebHostBuilder builder, string applicationName, string dvinAppId) {
-        ContainerBuilder containerBuilder = new ContainerBuilder().UseDvinAndPegh(applicationName);
-        IContainer container = containerBuilder.Build();
-        IDvinRepository dvinRepository = container.Resolve<IDvinRepository>();
-
-        var errorsAndInfos = new ErrorsAndInfos();
-        DvinApp dvinApp = await dvinRepository.LoadAsync(dvinAppId, errorsAndInfos)
-            ?? throw new Exception($"Dvin app {dvinAppId} not found");
-
-        if (errorsAndInfos.AnyErrors()) {
-            throw new Exception(errorsAndInfos.ErrorsToString());
-        }
-
+        IDvinApp dvinApp = await DvinAppHelper.LookUpDvinAppAsync(applicationName, dvinAppId);
         builder.UseUrls($"http://localhost:{dvinApp.Port}");
         return builder;
-    }
-
-    public static void RunHost(this IHostBuilder builder) {
-        IHost host = builder.Build();
-        host.Run();
     }
 }
---
+++ src/Singletons/DvinAppHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Aspenlaub.Net.GitHub.CSharp.Dvin.Components;
+using Aspenlaub.Net.GitHub.CSharp.Dvin.Entities;
+using Aspenlaub.Net.GitHub.CSharp.Dvin.Interfaces;
+using Aspenlaub.Net.GitHub.CSharp.Skladasu.Entities;
+using Aspenlaub.Net.GitHub.CSharp.Skladasu.Extensions;
+using Autofac;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Dvin.Singletons;
+
+public static class DvinAppHelper {
+    public static IDvinApp LookUpDvinApp(string applicationName, string dvinAppId) {
+        return LookUpDvinAppAsync(applicationName, dvinAppId).Result;
+    }
+
+    public static async Task<IDvinApp> LookUpDvinAppAsync(string applicationName, string dvinAppId) {
+        ContainerBuilder containerBuilder = new ContainerBuilder().UseDvinAndPegh(applicationName);
+        IContainer container = containerBuilder.Build();
+        IDvinRepository dvinRepository = container.Resolve<IDvinRepository>();
+
+        var errorsAndInfos = new ErrorsAndInfos();
+        DvinApp dvinApp = await dvinRepository.LoadAsync(dvinAppId, errorsAndInfos)
+            ?? throw new Exception($"Dvin app {dvinAppId} not found");
+
+        return errorsAndInfos.AnyErrors()
+            ? throw new Exception(errorsAndInfos.ErrorsToString())
+            : dvinApp;
+    }
+}
