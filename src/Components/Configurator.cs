1d2abbfe8556c8ee309ac0b3074c81d0253cad34
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-20 16:51:05 +0100

    Move and rename

--- src/Components/Configurator.cs
+++ src/Components/Configurator.cs
@@ -5,15 +5,22 @@ using Aspenlaub.Net.GitHub.CSharp.Dvin.Interfaces;
 using Aspenlaub.Net.GitHub.CSharp.Skladasu.Entities;
 using Aspenlaub.Net.GitHub.CSharp.Skladasu.Extensions;
 using Autofac;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Aspenlaub.Net.GitHub.CSharp.Dvin.Components;
 
 public static class Configurator {
-    public static IDvinApp LookUpDvinApp(string applicationName, string dvinAppId) {
-        return LookUpDvinAppAsync(applicationName, dvinAppId).Result;
+    public static IWebHostBuilder ConfigureUrl(this IWebHostBuilder builder, string applicationName, string dvinAppId) {
+        return ConfigureUrlAsync(builder, applicationName, dvinAppId).Result;
     }
 
-    public static async Task<IDvinApp> LookUpDvinAppAsync(string applicationName, string dvinAppId) {
+    public static async Task<IWebHostBuilder> ConfigureUrlAsync(this IWebHostBuilder builder, string applicationName, string dvinAppId) {
+        IDvinApp dvinApp = await LookUpDvinAppAsync(applicationName, dvinAppId);
+        builder.UseUrls($"http://localhost:{dvinApp.Port}");
+        return builder;
+    }
+
+    private static async Task<IDvinApp> LookUpDvinAppAsync(string applicationName, string dvinAppId) {
         ContainerBuilder containerBuilder = new ContainerBuilder().UseDvinAndPegh(applicationName);
         IContainer container = containerBuilder.Build();
         IDvinRepository dvinRepository = container.Resolve<IDvinRepository>();
